34785aa3ce698c66cced37a5bef110607cfa8054
Oli G <oli@Seneca.fritz.box>
2022-06-05 18:10:09 +0200

    delete API added

--- app.py
+++ app.py
@@ -1,6 +1,60 @@
-from flask import Flask
+from http import HTTPStatus
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+recipes = [
+    {"id": 1, "name": "Egg Salad", "description": "this is an egg salad recipe"},
+    {"id": 2, "name": "tomato pasta", "description": "this is a lovely pasta recipe"},
+]
+
+
+@app.route("/recipes", methods=["GET"])
+def get_recipes():
+    return jsonify({"data": recipes})
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["GET"])
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] == recipe_id), None)
+    if recipe:
+        return jsonify(recipe)
+    return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
+
+
+@app.route("/recipes", methods=["POST"])
+def create_recipe():
+    data = request.get_json()
+
+    name = data.get("name")
+    description = data.get("description")
+
+    recipe = {"id": len(recipes) + 1, "name": name, "description": description}
+
+    recipes.append(recipe)
+    return jsonify(recipe), HTTPStatus.CREATED
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["PUT"])
+def update_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] == recipe_id), None)
+    if not recipe:
+        return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
+
+    data = request.get_json()
+    recipe.update({"name": data.get("name"), "description": data.get("description")})
+    return jsonify(recipe)
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["DELETE"])
+def delete_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] == recipe_id), None)
+    if not recipe:
+        return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
+
+    recipes.remove(recipe)
+    return jsonify(recipe), HTTPStatus.ACCEPTED
 
 
 @app.route("/")
